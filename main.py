f951dc4de7a2833336f956ac748f9c2e76d65d89
3rr1s <errisjusufi06@gmail.com>
2025-04-22 09:17:07 +0000

    adedd code for patient data

--- main.py
+++ main.py
@@ -5,3 +5,5 @@ from typing import List, Dict
 # -------------------------------
 # Patient Class with Logical Expr
 # -------------------------------
+class Patient:
+  def __init__(self, id: str, name: str, age: int, illness: str, score:
